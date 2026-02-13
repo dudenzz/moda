734345f2b1d3ebcccd0246536bc37ed0de32416e
root <root@DESKTOP-AHFJQ66.localdomain>
2026-02-13 07:40:25 +0100

    merging changes from main

--- moda/MCHV.h
+++
@@ -1,8 +0,0 @@
-#include <tuple>
-#include "Point.h"
-#include "NDTree.h"
-namespace moda {
-	namespace backend {
-		std::tuple<DType,DType,DType> solveMCHV(std::vector <Point*>& allSolutions, Point& idealPoint, Point& nadirPoint, clock_t maxTime, std::vector <MCHVResult*>& results, int numberOfObjectives);
-	}
-}
